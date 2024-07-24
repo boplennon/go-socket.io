b8eeca788aec55d777b3e0c6aea248fc0c6d545d
Bob <bop.lennon@gmail.com>
2024-07-24 10:57:12 +0700

    Update client.go

--- client.go
+++ client.go
@@ -7,8 +7,6 @@ import (
 	"strings"
 
 	"github.com/googollee/go-socket.io/engineio"
-	"github.com/googollee/go-socket.io/engineio/transport"
-	"github.com/googollee/go-socket.io/engineio/transport/polling"
 	"github.com/googollee/go-socket.io/logger"
 	"github.com/googollee/go-socket.io/parser"
 )
@@ -65,7 +63,7 @@ func fmtNS(ns string) string {
 
 func (c *Client) Connect() error {
 	dialer := engineio.Dialer{
-		Transports: c.opts,
+		Transports: c.opts.Transports,
 	}
 
 	enginioCon, err := dialer.Dial(c.url, nil)
